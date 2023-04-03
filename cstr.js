f5585bbe48cd7de841eb9302723e7a220a92b422
chemical-dumbneer <jeanpiresdecarlos@gmail.com>
2023-04-03 18:48:01 -0300

    ???

--- cstr.js
+++ cstr.js
@@ -1,17 +1,25 @@
+class Linha{
+    constructor(T0,Q0,CA0,CB0,CC0,CD0){
+        this.Temp = T0;
+        this.Vaz = Q0;
+        this.Conc = [[CA0,CB0,null,null],[null,null,CC0,CD0]];
+    };
+};
+
 class nó_mistura{
     entradas(vazão_1, vazão_2){
         this.e1 = parsefloat(vazão_1);
         this.e2 = parsefloat(vazão_2);
-        this.vazão_saída = e1 + e2;
-    }
+        this.vazão_saída = this.e1 + this.e2;
+    };
     
 };
 
 class reator{
     constructor(altura,raio){
-        this.raio = raio
-        this.altura = altura
-        this.volume_tot = altura * Math.pow(raio,2) * Math.PI;
+        this.raio = parsefloat(raio);
+        this.altura = parsefloat(altura)
+        this.volume_disp = altura * Math.pow(raio,2) * Math.PI;
     };
     vazão_saída (contr_vaz){
         this.vazão = contr_vaz
