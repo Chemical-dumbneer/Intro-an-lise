a1a7d646f71a48ca8fce32ffbb43929200f4f140
chemical-dumbneer <jeanpiresdecarlos@gmail.com>
2023-04-20 23:35:23 -0300

    inclusão de mais código na classe reator e troca das funções foreach

--- cstr.ts
+++ cstr.ts
@@ -41,9 +41,9 @@ class Nó_Mistura{ // Mistura as informações de duas linhas distintas e armaze
                                                                                             ponderada pela vazção das temperaturas das
                                                                                             linhas de entrada
                                                                                             */                                          
-        this.Conc.forEach(function(index:number){
-            this.Conc[index] = ((entrada1.Conc[index]*entrada1.Vaz) + (entrada2.Conc[index]*entrada2.Vaz))/this.Vaz;
-        });     /* define, para cada (for each) elemento da matriz de concentrações, que a concentração inicial elementar do nó é
+        for(let j = 0; j < this.Conc.length; j++){
+            this.Conc[j] = ((entrada1.Conc[j]*entrada1.Vaz) + (entrada2.Conc[j] * entrada2.Vaz)) / this.Vaz;
+        };    /* define, para cada (for each) elemento da matriz de concentrações, que a concentração inicial elementar do nó é
                 uma média ponderada pelas vazões das concentrações elementares das linhas-fonte deste nó */
         this.Fonte1 = entrada1; // define qual objeto é a fonte da entrada #1 deste nó
         this.Fonte2 = entrada2; // define qual objeto é a fonte da entrada #2 deste nó
@@ -53,9 +53,9 @@ class Nó_Mistura{ // Mistura as informações de duas linhas distintas e armaze
 
         this.Vaz = this.Fonte1.Vaz + this.Fonte2.Vaz;
         this.Temp = ((this.Fonte1.Temp*this.Fonte1.Vaz)+(this.Fonte2.Temp*this.Fonte2.Vaz))/this.Vaz;
-        this.Conc.forEach(function(index:number){
-            this.Conc[index] = ((this.Fonte1.Conc[index]*this.Fonte1.Vaz) + (this.Fonte2.Conc[index]*this.Fonte2.Vaz))/this.Vaz;
-        });
+        for (let j = 0; j < this.Conc.length; j++){
+            this.Conc[j] = ((this.Fonte1.Conc[j]*this.Fonte1.Vaz)+(this.Fonte2.Conc[j]*this.Fonte2.Vaz))/this.Vaz;
+        };
     };
 };
 
@@ -133,16 +133,23 @@ class CSTR_C_Jaqueta{   /* recebe informações de entrada, executa balanços de
     public His_Vol:number[];
     public Fonte_Alimentação:Linha;
     public Fonte_Jaqueta:Linha;
+    public Temp_Jaqueta:number;
     public Vol:number;
     public Raz_vaz:number;
     private Vaz_max:number;
     private Vol_max:number;
     private alfa:number;
     private altura_reator:number;
+
+    private Vol_Camisa:number;
     private A_tt:number;
+    private U_tt:number;
 
     private saldo_energia:number;
-    private saldo_volume: number;
+    private Mol_Reator:number[];
+    private Mol_Entrada:number[];
+    private Mol_Saida:number[];
+    private Mol_Reação:number[];
     private saldo_molar:number[];
 
     private Dados_Reação = {
@@ -154,22 +161,28 @@ class CSTR_C_Jaqueta{   /* recebe informações de entrada, executa balanços de
     };
     
     K_arr(Temp:number){
-        return this.Dados_Reação.A * Math.exp((-this.Dados_Reação.Ej)/(8.314 * (Temp + 273.15)));
+        return this.Dados_Reação.A * Math.exp((-this.Dados_Reação.Ej)/(8.314 * (Temp)));
     };  
 
     constructor(Fonte_Alimentação:Linha, Fonte_Jaqueta:Linha, Raz_Vol_in:number, 
         Raz_vaz_in:number, Raio:number, Altura:number, Raz_Cobertura_Jaqueta:number,
-        Temp_in:number, Conc_in:number[]){
+        Espessura_Da_Camisa:number, Temp_in:number, Conc_in:number[]){
 
         this.Fonte_Alimentação = Fonte_Alimentação;
         this.Fonte_Jaqueta = Fonte_Jaqueta;
+        this.Temp_Jaqueta = Fonte_Jaqueta.Temp;
+
+        this.A_tt = Raz_Cobertura_Jaqueta * Altura * 2 * Math.PI * Raio;
+        this.Vol_Camisa = Math.PI * ((2 * Raio * Espessura_Da_Camisa) + (Math.pow(Espessura_Da_Camisa , 2)));
+        this.U_tt = 368; // vide https://www.tlv.com/global/BR/steam-theory/overall-heat-transfer-coefficient.html para mais info
+
         this.Vol_max = Altura * Math.PI * Math.pow(Raio,2);
         this.Vol = this.Vol_max * Raz_Vol_in;
         this.altura_reator = Altura;
         
         this.His_Vol[i] = this.Vol;
         this.His_Temp[i] = Temp_in;
-        this.Temp = Temp_in;
+        this.Temp = Temp_in + 273.15;
         
         this.alfa = Math.PI * Math.pow(0.1 * Raio,2) * Math.sqrt(2*9.81);
         this.Vaz_max = this.alfa * Math.sqrt(this.altura_reator * (this.Vol/this.Vol_max));
@@ -178,15 +191,30 @@ class CSTR_C_Jaqueta{   /* recebe informações de entrada, executa balanços de
         this.His_Vaz[i] = this.Vaz;
         this.His_Conc[i] = Conc_in;
 
-
     };  
 
-    Bal_Massa():void{
-
+   Bal_Massa():void{
+        
         this.Vol = this.His_Vol[i-1] + (this.Fonte_Alimentação.Vaz - this.Vaz)*dt;
+        for(let j = 0; j < this.Conc.length; j++){
+            this.Mol_Reator[j] = this.Conc[j] * this.Vol;
+            this.Mol_Entrada[j] = this.Fonte_Alimentação.Conc[j] * this.Fonte_Alimentação.Vaz * dt;
+            this.Mol_Saida[j] = this.Conc[j] * this.Vaz * dt;
 
-        
-        
+            this.Mol_Reação[j] = this.K_arr(this.Temp) * dt;
+            for(let k = 0; k < this.Dados_Reação.matriz_reagente.length; k++){
+                if(this.Dados_Reação.matriz_reagente[k] < 0){
+                    this.Mol_Reação[j] = this.Mol_Reação[j] * Math.pow(this.Conc[k], - this.Dados_Reação.matriz_reagente[k]);
+                };
+            };
+            this.saldo_molar[j] = this.Mol_Entrada[j] - this.Mol_Saida[j] + (this.Mol_Reação[j] * this.Dados_Reação.matriz_reagente[j] /
+Math.abs(this.Dados_Reação.matriz_reagente[j]));
+            
+            this.Mol_Reator[j] = this.Mol_Reator[j] + this.saldo_molar[j];
+            this.Conc[j] = this.Mol_Reator[j] / this.Vol;
+        };
+        this.His_Conc[i] = this.Conc;
+
     };
 
     Bal_Energia():void{
