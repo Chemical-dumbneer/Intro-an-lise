1a1876fa0123b372d65060d951ab9f03c101af0d
chemical-dumbneer <jeanpiresdecarlos@gmail.com>
2023-07-23 02:42:10 -0300

    just some updates

--- intro 2.0/Objetos_2.0.py
+++ intro 2.0/Objetos_2.0.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from tqdm.auto import tqdm
 
-class Info:
+class Info(object):
     def __init__(self, Nomes_reag:list, Densidade:float, Cp:float) -> None:
         """
         Informações sobre o fluído reativo do sistema
@@ -39,13 +39,13 @@ class Linha:
         self.Cs_Temp = self.Fonte.Cs_Temp
         self.Cs_Conc = self.Fonte.Cs_Conc
 
-    def Register(self, i) -> None:
+    def Vectorize(self) -> None:
         pass
 
     def Publish(self) -> None:
         pass
 
-class Fonte:
+class Fonte(object):
     def __init__(self, Vaz_max, Raz_vaz, Temp, Conc:list) -> None:
         
         self.Vaz_Max = Vaz_max/1000
@@ -62,23 +62,95 @@ class Fonte:
         self.Cs_Vaz[0] = self.Raz_Vaz[0] * self.Vaz_Max
         self.Output.Update()
     
-    def Register(self, i) -> None:
+    def Vectorize(self) -> None:
         pass
 
     def Publish(self) -> None:
         pass
         
-class Nó_Mistura:
+class Nó_2_p_1(object):
+    def Calc_Temp(self) -> None:
+        if self.Cs_Vaz == 0:
+            self.Cs_Temp = 0
+        else:
+            self.Cs_Temp = ((self.Fonte_1.Cs_Vaz * self.Fonte_1.Cs_Temp) + (self.Fonte_2.Cs_Vaz * self.Fonte_2.Cs_Temp))/self.Cs_Vaz
+
+    def Calc_Conc(self, init:bool) -> None:
+        if init:
+            self.Cs_Conc = []
+
+        for j in range(0, len(self.Fonte_1.Cs_Conc), 1):
+            Conc = ((self.Fonte_1.Cs_Conc[j] * self.Fonte_1.Cs_Vaz) + (self.Fonte_2.Cs_Conc[j] * self.Fonte_2.Cs_Vaz))/self.Cs_Vaz    
+            if init:
+                self.Cs_Conc.append(Conc)
+            else:
+                self.Cs_Conc[j] = Conc
+            
     def __init__(self, Entrada_1:Linha, Entrada_2:Linha) -> None:
         
         self.Fonte_1 = Entrada_1
         self.Fonte_2 = Entrada_2
         
         self.Cs_Vaz = Entrada_1.Cs_Vaz + Entrada_2.Cs_Vaz
-        if(self.Cs_Vaz == 0):
-            self.Cs_Temp = 0
-        else:
-            self.Cs_Temp = ((Entrada_1.Cs_Vaz*Entrada_1.Cs_Temp) + (Entrada_2.Cs_Vaz*Entrada_2.Cs_Temp))/self.Cs_Vaz
         
-        for j in range(0,len(Entrada_1.Cs_Conc),1):
+        self.Calc_Temp()
+        self.Calc_Conc(True)
+
+        self.Saída = Linha(self)
+
+    def Update(self) -> None:
+        
+        self.Cs_Vaz = self.Fonte_1.Cs_Vaz + self.Fonte_2.Cs_Vaz
+        self.Calc_Temp()
+        self.Calc_Conc(False)
+        
+        self.Saída.Update()
+
+    def Vectorize(self) -> None:
+        pass
+
+    def Publish(self) -> None:
+        pass
+
+class Nó_1_p_2(object):
+    class Saída(object):
+        def __init__(self, Base, Raz) -> None:
+
+            self.Base = Base
+            self.Raz = Raz    
+            self.Cs_Vaz = self.Raz * self.Base.Cs_Vaz
+            self.Cs_Temp = self.Base.Cs_Temp
+            self.Cs_Conc = self.Base.Cs_Conc
+        
+        def Update(self) -> None:
             
+            self.Cs_Vaz = self.Raz * self.Base.Cs_Vaz
+            self.Cs_Temp = self.Base.Cs_Temp
+            self.Cs_Conc = self.Base.Cs_Conc
+
+    def __init__(self, Entrada:Linha, Raz_Vaz_2_a_1:float) -> None:
+        
+        self.Cs_Saída_1 = self.Saída(self, 1 - Raz_Vaz_2_a_1)
+        self.Cs_Saída_2 = self.Saída(self, Raz_Vaz_2_a_1)
+
+        self.Saída_1 = Linha(self.Cs_Saída_1)
+        self.Saída_2 = Linha(self.Cs_Saída_2)
+    
+    def Update(self) -> None:
+
+        self.Cs_Saída_1.Update()
+        self.Cs_Saída_2.Update()
+
+        self.Saída_1.Update()
+        self.Saída_2.Update()
+    
+    def Vectorize(self) -> None:
+        pass
+
+    def Publish(self) -> None:
+        pass
+
+class Reação(object):
+    def __init__(self) -> None:
+        
+    
